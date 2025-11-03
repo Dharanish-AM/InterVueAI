8fbbd29ee0988b9bdf29983a087a75d4afb4bbbe
Dharanish A M <dharanish816@gmail.com>
2025-11-03 22:20:03 +0530

    Remove STT and TTS modules and update main.py
    Deleted unused speech-to-text and text-to-speech API and service files. Cleaned up main.py imports and formatting to reflect these removals.

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,12 +1,15 @@
 from app.api import evaluation
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI  # type: ignore
 from app.api import resume, questions
 
 app = FastAPI(title="InterVueAI")
 
 app.include_router(resume.router, prefix="/api/resume", tags=["Resume Parser"])
 app.include_router(questions.router, prefix="/api/questions", tags=["Question Engine"])
-app.include_router(evaluation.router, prefix="/api/evaluation", tags=["Evaluation Engine"])
+app.include_router(
+    evaluation.router, prefix="/api/evaluation", tags=["Evaluation Engine"]
+)
+
 
 @app.get("/")
 def root():
